5f3ee1cd14a05fa1326159bd68bbe1d71d641b2e
Piotr Niełacny <piotr.nielacny@gmail.com>
2021-02-10 14:32:35 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+gcloud functions deploy delivery --runtime python38 --trigger-http --allow-unauthenticated \
+	 --set-env-vars EMAIL=$EMAIL \
+	 --set-env-vars PASSWORD=$PASSOWRD \
+	 --set-env-vars HEADLESS=$HEADLESS  \
+	 --project $PROJECT \
+	 --region $REGION 
