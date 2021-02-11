fdc7b5ae006ef27a7bcd7386cd91197112a33041
Piotr Niełacny <piotr.nielacny@gmail.com>
2021-02-11 18:08:06 +0100

    Use substitution in cloudbuild

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 gcloud functions deploy delivery --runtime python38 --trigger-http --allow-unauthenticated \
-	 --set-env-vars EMAIL=$_EMAIL \
-	 --set-env-vars PASSWORD=$_PASSOWRD \
-	 --set-env-vars HEADLESS=$_HEADLESS  \
-	 --project $_PROJECT \
-	 --region $_REGION 
+	 --set-env-vars EMAIL=$EMAIL \
+	 --set-env-vars PASSWORD=$PASSOWRD \
+	 --set-env-vars HEADLESS=$HEADLESS  \
+	 --project $PROJECT \
+	 --region $REGION 
